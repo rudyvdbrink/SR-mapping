a3d92d1c8f8833d0005d607d7ed4f710b526091e
Ruud (Rudy) van den Brink <30622165+rudyvdbrink@users.noreply.github.com>
2022-09-29 11:33:31 +0200

    Figure 5 init

---
+++ Figure_5/Fig5d.m
@@ -0,0 +1,56 @@
+%% clear contents and add function folder with subfolders
+
+clear
+close all
+clc
+
+homedir = mfilename('fullpath');
+funcdir = [homedir(1:end-14) 'functions'];
+addpath(genpath(funcdir))
+
+%% load the data
+
+load data_fig5d.mat
+
+% Relevant variables are:
+% intrinsic_matrix_inferred: stimulus and action decoder output correlation computed on ongoing activity. This is the same is the outlined region in Figure 5c
+% evoked_matrix_inferred: stimulus and action decoder output correlation computed on evoked activity.
+% r_null_inf: Correlation of the evoked responses between the first and second half of data (serves as the null distribution for comparison)
+
+%% correlate vectorized evoked and intrinsic correlation matrices
+
+r_all = nan(size(evoked_matrix_inferred,1),1);
+for subi = 1:size(evoked_matrix_inferred,1)
+    a = evoked_matrix_inferred(subi,:)';
+    b = intrinsic_matrix_inferred(subi,:)';    
+    r_all(subi) = corr(a,b);    
+end
+
+%% plot correlation between evoked and intrinsic correlations and compare to null
+
+figure
+
+a = r_all; %correlation of evoked responses and iFC
+b = r_null_inf; %mean across-trial consistency of correlation of evoked responses
+
+plot(0.5,squeeze(a),'wo','MarkerFaceColor',[1 1 1]*.5)
+hold on
+plot(2.5,squeeze(b),'wo','MarkerFaceColor',[1 1 1]*.5)
+plot([0.5 2.5],[a b],'Color',[1 1 1]*.5)
+bar(nanmean([a b]),'EdgeColor','none')
+wse([a b],1);
+ylim([-0.4 1])
+
+box off
+set(gca,'tickdir','out')
+set(gca,'fontsize',15,'xtick',[])
+
+stats = permtestnds(a, zeros(size(a)), 10000,0.05,'both');
+text(1,0.1,['p = ' num2str(stats.p)])
+
+stats = permtestnds(b, zeros(size(b)), 10000,0.05,'both');
+text(2,0.9,['p = ' num2str(stats.p)])
+
+stats = permtestnds(a, b, 10000,0.05,'both');
+title(['p = ' num2str(stats.p)])
+
---
+++ Figure_5/Fig5f.m
@@ -0,0 +1,47 @@
+%% clear contents and add function folder with subfolders
+
+clear
+close all
+clc
+
+homedir = mfilename('fullpath');
+funcdir = [homedir(1:end-14) 'functions'];
+addpath(genpath(funcdir))
+
+%% load the data
+
+load data_fig5f.mat
+
+% Relevant variables are:
+% conn_inf: Correlation of stimulus and action decoders, averaged across all visual-motor ROI pairs.
+% conn_inf: Correlation of stimulus and action decoders, averaged across all auditory-motor ROI pairs. Here the stimulus decoder was trained and projected onto auditory cortex (as a control)
+
+%% compare and plot 
+figure
+
+conn = conn_inf-conn_audi_inf;
+
+plot(0.5,squeeze(conn(:,1)),'wo','MarkerFaceColor',[1 1 1]*.5)
+hold on
+plot(2.5,squeeze(conn(:,2)),'wo','MarkerFaceColor',[1 1 1]*.5)
+plot([0.5 2.5],conn,'Color',[1 1 1]*.5)
+box off
+set(gca,'tickdir','out')
+bar(squeeze(rmean(conn)),'EdgeColor','none')
+set(gca,'fontsize',15,'xtick',[])
+
+wse(conn(~isnan(sum(conn,2)),:),1); %error bars
+
+stats = permtestnds(conn(:,1), zeros(size(conn(:,1))), 10000,0.05,'right');
+text(1,0.02,['p = ' num2str(stats.p)],'HorizontalAlignment','center')
+
+stats = permtestnds(conn(:,2), zeros(size(conn(:,2))), 10000,0.05,'left');
+text(2,-0.02,['p = ' num2str(stats.p)],'HorizontalAlignment','center')
+
+stats = permtestnds(conn(:,1), conn(:,2), 10000,0.05,'right');
+title(['p = ' num2str(stats.p)])
+
+set(gca,'fontsize',15,'xtick',1:2)
+xlabel('Rule')
+
+
